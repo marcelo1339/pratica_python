a7f5956ac8b718d53edf75bce0a61f9d367778e5
Marcelo <marceloamaral1364@gmail.com>
2023-01-25 14:07:40 -0300

    up1

--- pc_adivinhaonumero/interacao.py
+++ pc_adivinhaonumero/interacao.py
@@ -6,7 +6,7 @@ from time import sleep
 usu = int()
 num = randint(-1000, 1000)
 interrompe = False
-print(num)
+
 
 linha()
 print('O computador esconde um número, seu objetivo é adivinhá-lo!'.center(80))
@@ -14,7 +14,7 @@ linha()
 
 print(f'{formatacao("sublinhado", "-1000 <= Valor <= 1000")}'.rjust(87))
 
-while usu != num or not interrompe:
+while not interrompe:
     try:
         usu = int(input('Sua tentativa>> '))
     except (ValueError, TypeError):
@@ -26,19 +26,28 @@ while usu != num or not interrompe:
         print('O programa foi encerrado durante sua execução.')
         break
 
+    if usu == num:
+        break
+
     if num >= 0:
         if num < usu <= num + 100:
             linha()
-            print(f'>>{formatacao("vermelho", "Quente!!")} Tente um número menor')
+            print(f'>>{formatacao("vermelho", "Quente!!")} Você tentou o número {usu}. Tente um número menor')
             linha()
         elif num > usu >= num - 100:
             linha()
-            print(f'>>{formatacao("verde", "Talvez você queira tentar un número maior...")}')
+            print(f'>> Seu número foi {usu}. {formatacao("verde", "Talvez você queira tentar un número maior...")}')
             linha()
     elif num < 0:
         if num > usu >= num - 100:
             linha()
-            print(f'>>{formatacao("vermelho", "Tá perto!!")} Tente um número maior')
+            print(f'>>{formatacao("vermelho", "Tá perto!!")} Seu número foi {usu}. Tente um número maior')
             linha()
         elif num < usu < num + 100:
-            print(f'{formatacao("verde", "Talvez você queira tentar um número menor...")}')
+            linha()
+            print(f'>> Seu número foi {usu}. {formatacao("verde", "Talvez você queira tentar um número menor...")}')
+            linha()
+
+if usu == num:
+    sleep(1)
+    print(f'{formatacao("verde", "Parabéns!! Você acertou o número: ")} {num}.')
