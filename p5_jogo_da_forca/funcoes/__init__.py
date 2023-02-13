b02bb4b4f316f2e75d95e49fc1d825ce28a2d08c
Marcelo <marceloamaral1364@gmail.com>
2023-02-12 21:06:18 -0300

    Dia 2

---
+++ p5_jogo_da_forca/funcoes/__init__.py
@@ -0,0 +1,42 @@
+from random import randint, choice
+
+
+def menos_palavras(s, origem_txt):
+    """
+    A ideia é diminuir uma grande quantidade de palavras de um txt.
+    >>precisa das funções 'choice' e 'randint' do módulo 'random' para funcionar.<<
+
+    :param s: Recebe um set
+    :param origem_txt: Recebe o arquivo de texto que contêm a lista de palavras
+    :return: retorna a palavra que vai ser usada
+    """
+
+    print('Aguarde! Sua palavra está sendo selecionada...')
+
+    palavra = str()
+    novas_palavras = list()
+    cont = aleatorio = 0
+    alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    for letra in alfabeto:
+        with open(origem_txt, 'r', encoding='utf-8') as plv:
+            for p in plv:
+                cont = 0
+                aleatorio = randint(0, 1)
+                if (p.upper()[0] == letra) and ('-' not in p) and ('.' not in p) and (' ' not in p) and (aleatorio == 1) \
+                        and len(p) > 4 and (cont <= 3000):
+                    s.add(p.lower())
+                    cont += 1
+
+    for p in s:
+        novas_palavras.append(p)
+
+    palavra = choice(novas_palavras)
+
+    return palavra
+
+
+def linha():
+    print('-=' * 30)
+
--- p5_jogo_da_forca/jogo.py
+++ p5_jogo_da_forca/jogo.py
@@ -1,15 +1,39 @@
 """Todas as palavras que estão no arquivo -palavras.txt- foram retiradas do perfil Python Pro:
 https://github.com/pythonprobr/palavras"""
-from random import choice
 
-palavras = list()
-
-alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-            'V', 'W', 'X', 'Y', 'Z']
-
-with open('palavras.txt', 'r', encoding='utf-8') as plv:
-    for letra in alfabeto:
-        for c in range(0, 1000):
+from p5_jogo_da_forca.funcoes import *
+from time import sleep
 
 
-print(palavras)
+cabeca = tronco = perna_esq = perna_dir = braco_esq = braco_dir = olhos = nariz = boca = interrompe = False
+palavras = set()
+usu = str()
+word = menos_palavras(palavras, 'palavras.txt')
+
+
+while True:
+    if (cabeca and tronco and perna_esq and perna_dir and braco_esq and braco_dir and olhos and nariz and boca) \
+            or interrompe:
+        break
+
+
+    linha()
+    print('Jogo da forca'.center(60))
+    linha()
+
+    while True:
+        try:
+            linha()
+            print(f'A palavra tem {len(word)} letras.')
+            usu = str(input('Tente uma letra: ')).strip().upper()[0]
+        except (ValueError, TypeError, IndexError):
+            print('Por favor, tente uma letra!')
+        except KeyboardInterrupt:
+            print('\nEncerrando...')
+            interrompe = True
+            print('Programa encerrado')
+            break
+
+
+
+"""(primeiro a cabeça, depois o tronco, de seguida pernas e braços e termina-se com olhos, nariz e boca)"""
