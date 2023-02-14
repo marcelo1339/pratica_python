16c90da2002ed9432813e183bfc02d9c1165d043
Marcelo <marceloamaral1364@gmail.com>
2023-02-14 10:37:07 -0300

    Dia 3

--- p5_jogo_da_forca/funcoes/__init__.py
+++ p5_jogo_da_forca/funcoes/__init__.py
@@ -1,18 +1,18 @@
 from random import randint, choice
 
 
-def menos_palavras(s, origem_txt):
+def palavra_txt(origem_txt):
     """
     A ideia é diminuir uma grande quantidade de palavras de um txt.
     >>precisa das funções 'choice' e 'randint' do módulo 'random' para funcionar.<<
 
-    :param s: Recebe um set
     :param origem_txt: Recebe o arquivo de texto que contêm a lista de palavras
     :return: retorna a palavra que vai ser usada
     """
 
     print('Aguarde! Sua palavra está sendo selecionada...')
 
+    s = set()
     palavra = str()
     novas_palavras = list()
     cont = aleatorio = 0
@@ -25,7 +25,7 @@ def menos_palavras(s, origem_txt):
                 cont = 0
                 aleatorio = randint(0, 1)
                 if (p.upper()[0] == letra) and ('-' not in p) and ('.' not in p) and (' ' not in p) and (aleatorio == 1) \
-                        and len(p) > 4 and (cont <= 3000):
+                        and len(p) > 4 and (cont <= 1000):
                     s.add(p.lower())
                     cont += 1
 
@@ -33,10 +33,8 @@ def menos_palavras(s, origem_txt):
         novas_palavras.append(p)
 
     palavra = choice(novas_palavras)
-
     return palavra
 
 
 def linha():
     print('-=' * 30)
-
--- p5_jogo_da_forca/jogo.py
+++ p5_jogo_da_forca/jogo.py
@@ -8,7 +8,9 @@ from time import sleep
 cabeca = tronco = perna_esq = perna_dir = braco_esq = braco_dir = olhos = nariz = boca = interrompe = False
 palavras = set()
 usu = str()
-word = menos_palavras(palavras, 'palavras.txt')
+plvs_usadas = list()
+cont = 0
+word = palavra_txt('palavras.txt')
 
 
 while True:
@@ -23,13 +25,51 @@ while True:
 
     while True:
         try:
+            if cabeca and tronco and perna_esq and perna_dir and braco_esq and braco_dir and olhos and nariz and boca:
+                print(' :-( Parece que você perdeu!')
+                break
+
             linha()
-            print(f'A palavra tem {len(word)} letras.')
-            usu = str(input('Tente uma letra: ')).strip().upper()[0]
+            print(word)
+            print(f'--Você tem {9 - cont} tentativas.')
+            print(f'--A palavra tem {len(word)-1} letras.')
+            usu = str(input('>>Tente uma letra: ')).strip().upper()[0]
+            if not usu.isalpha():
+                print('Por favor, tente uma letra!')
+
+            if usu.upper() not in word.upper() and usu.isalpha():
+                if usu.upper() not in plvs_usadas:
+                    plvs_usadas.append(usu)
+                cont += 1
+
+            if cont == 1:
+                cabeca = True
+            elif cont == 2:
+                tronco = True
+            elif cont == 3:
+                perna_esq = True
+            elif cont == 4:
+                perna_dir = True
+            elif cont == 5:
+                braco_dir = True
+            elif cont == 6:
+                braco_esq = True
+            elif cont == 7:
+                olhos = True
+            elif cont == 8:
+                nariz = True
+            elif cont == 9:
+                boca = True
+
+            print(plvs_usadas)
+            """(primeiro a cabeça, depois o tronco, de seguida pernas e braços e termina-se com olhos, nariz e boca)"""
+
+
         except (ValueError, TypeError, IndexError):
             print('Por favor, tente uma letra!')
         except KeyboardInterrupt:
             print('\nEncerrando...')
+            sleep(1)
             interrompe = True
             print('Programa encerrado')
             break
