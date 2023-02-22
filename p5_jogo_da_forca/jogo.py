78238ee743dee8f97b12772c7f80f70378514595
Marcelo <marceloamaral1364@gmail.com>
2023-02-22 11:41:07 -0300

    Dia 7

--- p5_jogo_da_forca/jogo.py
+++ p5_jogo_da_forca/jogo.py
@@ -9,10 +9,9 @@ interrompe = False
 usu = str()
 palavra = list()
 nao_existe = list()
-plvs_usadas = list()
-letras_repetidas = list()
 word = list()
-cont = c = indice_atual = 0
+indice_letra_repetida = list()
+cont = c = repetidas = letras_repetidas = 0
 
 for l in palavra_txt('palavras.txt'):
     word.append(l.lower())
@@ -26,10 +25,6 @@ while True:
     if cont == 9 or interrompe:
         break
 
-    linha()
-    print('Jogo da forca'.center(60))
-    linha()
-
     while True:
         try:
             if cont == 9:
@@ -37,11 +32,16 @@ while True:
                 break
 
             linha()
+            print('Jogo da forca'.center(60))
+            linha()
 
             print(word)
 
             print(f'--Você tem {9 - cont} tentativas.')
             print(f'--A palavra tem {len(word)} letras.')
+            print('--Lembre-se que a palavra pode ter letras com acentos \n como "í, é, â, õ" entre outras.')
+
+            linha()
             usu = str(input('>>Tente uma letra: ')).strip().lower()[0]
 
             if not usu.isalpha():
@@ -50,13 +50,24 @@ while True:
             if usu not in word and usu.isalpha():
                 if usu not in nao_existe:
                     nao_existe.append(usu)
-                print(nao_existe)
+                print(f'Suas tentativas: {nao_existe}')
                 cont += 1
 
             if usu in word:
-                indice_atual = word.index(usu, indice_atual)
-                print(indice_atual)
+                repetidas = word.count(usu)
+                if repetidas > 1:
+                    for pos, letra in enumerate(word):
+                        if (usu == letra) and (pos not in indice_letra_repetida):
+                            indice_letra_repetida.append(pos)
 
+                    for letra in word:
+                        for indice in indice_letra_repetida:
+                            if usu == letra:
+                                palavra.pop(indice)
+                                palavra.insert(indice, usu)
+                    indice_letra_repetida.clear()
+
+            print('Sua palavra: ', end='')
             for elemento in palavra:
                 print(elemento, end='')
             print()
